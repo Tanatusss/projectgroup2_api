2df1540ecef6bd2d0004f1bbdc1ae4ec17915abb
SirawitWong <sirawit.wong.tt@gmail.com>
2025-07-17 21:42:20 +0700

    add starter for server

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 
+const app = express();
+const PORT = 3000;
 
+app.use(express.json());
+app.use(cors({
+	origin: true,
+	methods: ["GET", "POST", "PATCH", "DELETE"],
+	allowedHeaders: ["Content-Type", "Authorization"],
+}));
+app.use(helmet());
 
-
+app.listen(PORT, () => {
+	console.log(`Server running at http://localhost:${PORT}`)
+});
