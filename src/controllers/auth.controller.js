6cdaa556ab7af9a527829255759c33c2289ac0c7
tanatusss <tanatus.v@hotmail.com>
2025-07-23 13:56:02 +0700

    testloginregister

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { createError } from "../utils/createError.js";
 import bcrypt, { compare } from 'bcryptjs';
 import prisma from '../config/prisma.js'
-import { createUser, findUser } from "../services/user.service.js";
+import { createCompany, createUser, findCompany, findUser } from "../services/user.service.js";
 import { signToken } from "../utils/jwtUtil.js";
 import crypto from "crypto";
 import nodemailer from "nodemailer";
@@ -51,6 +51,62 @@ export const loginUser = async(req,res,next)=>{
     const payload = {
       id: user.id,
       role: user.role
+    }
+    const token = signToken(payload)
+    console.log("token",token)
+    res.json({
+      msg: 'Login success',
+      token: token,
+    })
+
+  }catch(error){
+    next(error)
+  }
+}
+export const registerCompany = async(req,res,next) => {
+  try{
+    const {email, password, confirmPassword} = req.body;
+     if (!(email.trim() && password.trim() && confirmPassword.trim())) {
+    createError(400, 'Please fill all data')
+  }
+    if (password != confirmPassword){
+      createError(400, 'Please check confirm-password')
+    }
+  const user = await prisma.company.findUnique({
+      where:{
+        email:email,
+      }
+    })
+
+    if(user){
+      createError(400,'Email already exist!!!')
+    }
+  const hashPassword =  bcrypt.hashSync(password, 10)
+  console.log(password);
+  const newCompany = {
+    email,
+    password: hashPassword
+  } 
+  const result = await createCompany(newCompany)
+  res.json({message: `Register success`,result})
+  
+  }catch(error){
+    next(error)
+  }
+}
+
+export const loginCompany = async(req,res,next)=>{
+  try{
+    const {email,password} = req.body;
+    const company = await findCompany(email)
+    const pwOk = await bcrypt.compare(password, company.password)
+    if(!pwOk){
+      throw createError(401, 'Invalid Login')
+    }
+
+    const payload = {
+      id: company.id,
+      role: company.role
     }
     const token = signToken(payload)
     console.log("token",token)
