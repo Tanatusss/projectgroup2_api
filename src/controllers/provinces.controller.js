50b7a2364a2423dc1a3eba3756433db571124f18
tanatusss <tanatus.v@hotmail.com>
2025-07-25 14:53:35 +0700

    new seed

--- src/controllers/provinces.controller.js
+++ src/controllers/provinces.controller.js
@@ -40,3 +40,4 @@ export const searchProvinces = async (req, res) => {
     });
   }
 };
+
--- src/routes/experience.route.js
+++ src/routes/experience.route.js
@@ -1,9 +1,11 @@
 import express from "express"
+import { authCheck } from "../middlewares/authCheck.js"
+import { createWorkExperience } from "../controllers/experiece.controller.js"
 
 const experienceRouter = express.Router()
 
 experienceRouter.get("/profiles/:id/work-experiences", () => console.log("Get Experience By Id"))
-experienceRouter.post("/profiles/:id/work-experiences", () => console.log("Create Experience"))
+experienceRouter.post("/profiles/:id/work-experiences", authCheck,createWorkExperience)
 experienceRouter.patch("/work-experiences/:id", () => console.log("Edit Experience By Id"))
 experienceRouter.delete("/work-experiences/:id", () => console.log("Delete Experience By Id"))
 
